51b63c38c99e2019a8573908ae037c97629e1d26
Martin Stypinski <mstypinski@gmail.com>
2022-09-17 01:00:46 +0200

    added simple yield tokenizer

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+test_str = "There was a miller whose only inheritance to his three sons was his mill, his donkey, and his cat. The division was soon made. They hired neither a clerk nor an attorney, for they would have eaten up all the poor patrimony. The eldest took the mill, the second the donkey, and the youngest nothing but the cat."
+
+def sentence_tokenizer(text: str) -> str:
+    for sentence_token in text.split("."):
+        yield sentence_token
+
+
 def main():
-    pass
+    for sentence in sentence_tokenizer(test_str):
+        print(sentence)
+
+
 
 
 if __name__ == "__main__":
