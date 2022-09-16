61254af1a98ed8525e3a4189467779e149126c93
MarcoGlauser <github@mglauser.ch>
2022-09-17 08:18:27 +0900

    add fetching to sentence processing

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from fetch_images import fetch_image
 
 test_str = "There was a miller whose only inheritance to his three sons was his mill, his donkey, and his cat. The division was soon made. They hired neither a clerk nor an attorney, for they would have eaten up all the poor patrimony. The eldest took the mill, the second the donkey, and the youngest nothing but the cat."
 
@@ -8,9 +9,10 @@ def sentence_tokenizer(text: str) -> str:
 
 
 def main():
-    for sentence in sentence_tokenizer(test_str):
-        print(sentence)
-
+    for sentence_index, sentence in enumerate(sentence_tokenizer(test_str)):
+        images = fetch_image(sentence)
+        for index, image in enumerate(images):
+            image.save(f'images/{sentence_index:03d}-{index:03d}.png')
 
 
 
