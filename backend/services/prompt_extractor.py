7f1505a5b913a046c4cbc14b43f7665fd25a0522
marcelamsler <marcel.amsler@gmail.com>
2022-09-17 12:20:21 +0200

    improved extractor

--- backend/services/prompt_extractor.py
+++ backend/services/prompt_extractor.py
@@ -12,7 +12,7 @@ class PromptExtractor:
         self.style = style
 
     def _extract_prompt_with_nltk(self, text: str) -> str:
-        print("input: ", text)
+
         rake_nltk_var = Rake()
         rake_nltk_var.extract_keywords_from_text(text)
         keyword_extracted = rake_nltk_var.get_ranked_phrases()[0]
@@ -20,24 +20,29 @@ class PromptExtractor:
         return prompt
 
     def _extract_prompt_with_yake(self, text: str) -> str:
-        print("input: ", text)
         kw_extractor = yake.KeywordExtractor(n=5, dedupLim=0.9, top=2, features=None)
         extracted_words = " ".join(
             [kw_extractor.extract_keywords(text)[0][0], kw_extractor.extract_keywords(text)[1][0]])
-        print("extracted:", extracted_words)
         return extracted_words
 
     def extract_paragraphs_with_prompts(self, whole_text: str) -> Dict[str, str]:
         paragraph_prompts = {}
         paragraph = ""
-        line_with_content_counter = 0
-        for idx, line in enumerate(whole_text.splitlines()):
-            if line.strip():
-                line_with_content_counter += 1
-                paragraph += line
-            if line_with_content_counter == 3:
-                line_with_content_counter = 0
-                paragraph_prompts[paragraph] = self._extract_prompt_with_yake(
+        sentence_counter = 0
+        lines = whole_text.splitlines()
+        non_empty_lines = " ".join([line for line in lines if line.strip() != ""])
+        for idx, sentence in enumerate(non_empty_lines.split(".")):
+            sentence_counter += 1
+            paragraph += sentence.strip()
+            if sentence_counter == 5:
+                sentence_counter = 0
+                print("input: ", paragraph)
+                extracted_keywords = self._extract_prompt_with_yake(
                     paragraph) + " " + self._extract_prompt_with_nltk(paragraph)
+
+                words = extracted_keywords.lower().split()
+                deduplicated_prompt = " ".join(sorted(set(words), key=words.index))
+                print("extracted: ", deduplicated_prompt)
+                paragraph_prompts[paragraph] = deduplicated_prompt
                 paragraph = ""
         return paragraph_prompts
