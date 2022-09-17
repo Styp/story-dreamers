2106f5f7652f86b48ff7acf8b01eefd8aac9e5b6
Martin Stypinski <mstypinski@gmail.com>
2022-09-17 10:37:19 +0200

    StableDiffusion for FastAPI consumer

--- backend/serve.py
+++ backend/serve.py
@@ -1,11 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 
+from backend.serivces.stable_diffusion_consumer import StableDiffusionConsumer
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/imageForPromt")
+async def image_from_promt(prompt: str):
+    consumer = StableDiffusionConsumer()
+    return consumer.fetch_image(prompt)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
