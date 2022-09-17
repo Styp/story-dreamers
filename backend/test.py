f05541f3e85a3104da1002f90da4e982154194e6
Martin Stypinski <mstypinski@gmail.com>
2022-09-17 14:15:10 +0200

    addded auth for google vision api

--- backend/test.py
+++ backend/test.py
@@ -2,7 +2,7 @@ import os
 
 from google.cloud import vision
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-vision-key.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join('auth', 'google-vision-key.json')
 
 
 client = vision.ImageAnnotatorClient()
