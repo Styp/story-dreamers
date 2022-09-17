a9637ce2e7aa78bb83c39103b4364c2ad4a3edcd
Martin Stypinski <mstypinski@gmail.com>
2022-09-17 11:02:14 +0200

    draft of API

--- backend/serve.py
+++ backend/serve.py
@@ -12,12 +12,20 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/imageForPromt")
+@app.post("/imageFromPromt")
 async def image_from_promt(prompt: str):
     consumer = StableDiffusionConsumer()
     image_bytes = next(consumer.fetch_image(prompt))
 
     return Response(content=image_bytes, media_type="image/png")
+
+@app.post("/base64FromPrompt")
+async def base64_from_prompt(prompt: str) -> str:
+    pass
+
+@app.post("/promptsFromText")
+async def prompts_from_text(prompt: str) -> str:
+    pass
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
